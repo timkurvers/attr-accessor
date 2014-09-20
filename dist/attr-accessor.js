e527d678c273f128991427f4635bd75f17ddca52
Tim Kurvers <tim@moonsphere.net>
2014-09-21 00:15:57 +0200

    Bump to v0.1.1

--- dist/attr-accessor.js
+++ dist/attr-accessor.js
@@ -1,52 +1,65 @@
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.attr=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-var __slice = [].slice;
+var clone,
+  __hasProp = {}.hasOwnProperty;
+
+clone = function(object) {
+  var cloned, key, value;
+  cloned = {};
+  for (key in object) {
+    if (!__hasProp.call(object, key)) continue;
+    value = object[key];
+    cloned[key] = value;
+  }
+  return cloned;
+};
 
 module.exports = {
-  reader: function(object) {
+  reader: function(object, options) {
+    if (options == null) {
+      options = {};
+    }
+    options = clone(options);
+    if (options.configurable == null) {
+      options.configurable = true;
+    }
     return function(properties) {
       var getter, name;
       for (name in properties) {
         getter = properties[name];
-        Object.defineProperty(object, name, {
-          get: getter,
-          enumerable: true
-        });
+        options.get = getter;
+        Object.defineProperty(object, name, options);
       }
       return void 0;
     };
   },
-  writer: function(object) {
+  writer: function(object, options) {
+    if (options == null) {
+      options = {};
+    }
+    options = clone(options);
+    if (options.configurable == null) {
+      options.configurable = true;
+    }
     return function(properties) {
       var name, setter;
       for (name in properties) {
         setter = properties[name];
-        Object.defineProperty(object, name, {
-          set: setter,
-          enumerable: true
-        });
+        options.set = setter;
+        Object.defineProperty(object, name, options);
       }
       return void 0;
     };
   },
-  readers: function() {
-    var object, objects, _i, _len, _results;
-    objects = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-    _results = [];
-    for (_i = 0, _len = objects.length; _i < _len; _i++) {
-      object = objects[_i];
-      _results.push(this.reader(object));
+  accessor: function(object, options) {
+    var ioptions;
+    if (options == null) {
+      options = {};
     }
-    return _results;
-  },
-  writers: function() {
-    var object, objects, _i, _len, _results;
-    objects = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-    _results = [];
-    for (_i = 0, _len = objects.length; _i < _len; _i++) {
-      object = objects[_i];
-      _results.push(this.writer(object));
+    ioptions = clone(options);
+    if (ioptions.enumerable == null) {
+      ioptions.enumerable = true;
     }
-    return _results;
+    return [this.reader(object.prototype, ioptions), this.writer(object.prototype, ioptions), this.reader(object, options), this.writer(object, options)];
   }
 };
 
