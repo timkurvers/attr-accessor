58b6da76ba85340caddd79250af646c785d9b6d6
Tim Kurvers <tim@moonsphere.net>
2015-04-23 23:14:46 +0200

    Latest distribution files

--- dist/attr-accessor.js
+++ dist/attr-accessor.js
@@ -1,5 +1,5 @@
 /**
- * attr-accessor v0.2.0
+ * attr-accessor v0.2.1
  * Copyright (c) 2014 Tim Kurvers <tim@moonsphere.net>
  *
  * Convenience factories for creating getter/setters.
@@ -8,7 +8,7 @@
  */
 
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.attr=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-var clone,
+var clone, factory,
   __hasProp = {}.hasOwnProperty;
 
 clone = function(object) {
@@ -22,42 +22,34 @@ clone = function(object) {
   return cloned;
 };
 
+factory = function(object, options, method) {
+  options = clone(options);
+  if (options.configurable == null) {
+    options.configurable = true;
+  }
+  return function(properties) {
+    var accessor, name;
+    for (name in properties) {
+      accessor = properties[name];
+      options[method] = accessor;
+      Object.defineProperty(object, name, options);
+    }
+    return void 0;
+  };
+};
+
 module.exports = {
   reader: function(object, options) {
     if (options == null) {
       options = {};
     }
-    options = clone(options);
-    if (options.configurable == null) {
-      options.configurable = true;
-    }
-    return function(properties) {
-      var getter, name;
-      for (name in properties) {
-        getter = properties[name];
-        options.get = getter;
-        Object.defineProperty(object, name, options);
-      }
-      return void 0;
-    };
+    return factory(object, options, 'get');
   },
   writer: function(object, options) {
     if (options == null) {
       options = {};
     }
-    options = clone(options);
-    if (options.configurable == null) {
-      options.configurable = true;
-    }
-    return function(properties) {
-      var name, setter;
-      for (name in properties) {
-        setter = properties[name];
-        options.set = setter;
-        Object.defineProperty(object, name, options);
-      }
-      return void 0;
-    };
+    return factory(object, options, 'set');
   },
   accessor: function(object, options) {
     if (options == null) {
