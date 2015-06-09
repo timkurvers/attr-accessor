4fbdcafb26937a35f345cddac7c988bdf33f9517
Tim Kurvers <tim@moonsphere.net>
2015-06-09 23:13:22 +0200

    Latest browser release

--- dist/attr-accessor.js
+++ dist/attr-accessor.js
@@ -8,68 +8,58 @@
  */
 
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.attr=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-var clone, factory,
-  hasProp = {}.hasOwnProperty;
+'use strict';
 
-clone = function(object) {
-  var cloned, key, value;
-  cloned = {};
-  for (key in object) {
-    if (!hasProp.call(object, key)) continue;
-    value = object[key];
-    cloned[key] = value;
-  }
-  return cloned;
+var clone = function clone(object) {
+  return JSON.parse(JSON.stringify(object));
 };
 
-factory = function(object, options, method) {
+var factory = function factory(object, options, method) {
   options = clone(options);
-  if (options.configurable == null) {
+  if (options.configurable === undefined) {
     options.configurable = true;
   }
-  return function(properties) {
-    var accessor, name;
-    for (name in properties) {
-      accessor = properties[name];
+  return function (properties) {
+    for (var _name in properties) {
+      var accessor = properties[_name];
       options[method] = accessor;
-      Object.defineProperty(object, name, options);
+      Object.defineProperty(object, _name, options);
     }
-    return void 0;
+    undefined;
   };
 };
 
 module.exports = {
-  reader: function(object, options) {
-    if (options == null) {
-      options = {};
-    }
+
+  reader: function reader(object) {
+    var options = arguments[1] === undefined ? {} : arguments[1];
+
     return factory(object, options, 'get');
   },
-  writer: function(object, options) {
-    if (options == null) {
-      options = {};
-    }
+
+  writer: function writer(object) {
+    var options = arguments[1] === undefined ? {} : arguments[1];
+
     return factory(object, options, 'set');
   },
-  accessor: function(object, options) {
-    if (options == null) {
-      options = {};
-    }
+
+  accessor: function accessor(object) {
+    var options = arguments[1] === undefined ? {} : arguments[1];
+
     return [this.reader(object, options), this.writer(object, options)];
   },
-  accessors: function(object, options) {
-    var ioptions;
-    if (options == null) {
-      options = {};
-    }
-    ioptions = clone(options);
-    if (ioptions.enumerable == null) {
+
+  accessors: function accessors(object) {
+    var options = arguments[1] === undefined ? {} : arguments[1];
+
+    var ioptions = clone(options);
+    if (ioptions.enumerable === undefined) {
       ioptions.enumerable = true;
     }
     return this.accessor(object.prototype, ioptions).concat(this.accessor(object, options));
   }
-};
 
+};
 },{}]},{},[1])
 (1)
 });
